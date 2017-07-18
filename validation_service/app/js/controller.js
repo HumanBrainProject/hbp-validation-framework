5c73349af35db7e0246881b0b1a87d657b8d09b7
Joffrey Gonin <Joffrey.Gonin@gmail.com>
2017-07-18 16:13:03 +0200

    add ctx arg in post comment

--- validation_service/app/js/controller.js
+++ validation_service/app/js/controller.js
@@ -80,7 +80,7 @@ testApp.controller('ValTestDetailCtrl', ['$scope', '$rootScope', '$http', '$loca
 
             $scope.submit_comment = function() {
                 var data_comment = JSON.stringify({ author: $stateParams.uuid, text: this.text, approved_comment: false, test_id: $stateParams.uuid });
-                TestCommentRest.post(data_comment, function(value) {});
+                TestCommentRest.post({ ctx: CollabParameters.getCtx() }, data_comment, function(value) {});
             }
         });
 
