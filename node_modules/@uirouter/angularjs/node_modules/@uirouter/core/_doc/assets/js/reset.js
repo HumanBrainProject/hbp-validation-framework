6bfe3dccb42ae9df2c47644cde1d324f5fa84290
HFragnaud <helissande.fragnaud@gmail.com>
2018-08-09 13:53:19 +0200

    erase nodes modules

--- node_modules/@uirouter/angularjs/node_modules/@uirouter/core/_doc/assets/js/reset.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (window.localStorage) {
-  window.localStorage.removeItem('inherited');
-  window.localStorage.removeItem('only-exported');
-  window.localStorage.removeItem('visibility');
-}
