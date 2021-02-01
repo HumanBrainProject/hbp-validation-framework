baf20961419ad6914d451f07de2aa8537c73fe86
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2021-02-01 17:44:09 +0100

    fix problem when distribution digest is None

--- validation_service_v2/validation_service/data_models.py
+++ validation_service_v2/validation_service/data_models.py
@@ -205,7 +205,7 @@ class ModelInstance(BaseModel):
                     "source": main_script.code_location,
                     "license": main_script.license,
                     "script_id": main_script.id,  # internal use only
-                    "hash": main_script.distribution.digest
+                    "hash": getattr(main_script.distribution, "digest", None)
                 }
             )
         if instance.alternate_of:
