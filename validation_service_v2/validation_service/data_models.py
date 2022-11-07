d27c9232613e202e6be7c2633846bceb6bbb3fc9
Andrew Davison <andrew.davison@cnrs.fr>
2022-11-07 09:47:32 +0100

    Add ModelDB entry to alternatives (also for kg_query code paths)

--- validation_service_v2/validation_service/data_models.py
+++ validation_service_v2/validation_service/data_models.py
@@ -342,6 +342,8 @@ class ModelInstance(BaseModel):
             item["version"] = "unknown"
         if client.is_released(item["uri"]):
             item["alternatives"].append(f"https://search.kg.ebrains.eu/instances/{item['id']}")
+        if "modeldb" in item["source"].lower():
+            item["alternatives"].append(item["source"])
         return cls(**item)
 
     @classmethod
